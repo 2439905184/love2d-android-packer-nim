c97c6a2335dd08cb5501dbb910fdc016685dd352
小沙盒工作室-室长 <29478722+2439905184@users.noreply.github.com>
2022-08-03 21:24:54 +0800

    Add files via upload

---
+++ config.nim
@@ -0,0 +1,16 @@
+#请修改下面的变量以适用于你的电脑
+#安卓sdk根目录
+var android_sdk_root = "F:/sdk"
+#构建工具版本 路径如："F:\sdk\build-tools\28.0.3"
+var build_tool_version = "28.0.3"
+#模板apk的位置
+var release_template = "" 
+#游戏的名字
+var app_name = "love2d游戏测试"
+#签名文件路径
+var release_keystore = ""
+#签名文件账号密码
+var release_username = ""
+var release_password = ""
+#要使用的权限，请写入如android.permission.RECORD_AUDIO 这样子的参数名，写入表示开启此权限
+var used_permissions = []
